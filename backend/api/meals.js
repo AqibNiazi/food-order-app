ba9d3052d86d35033fcca713bbe0be5a8f069ae1
AqibNiazi <aqibjaved5201@gmail.com>
2025-06-16 11:01:47 +0500

    Meals.js file changed for vercel deployment

--- backend/api/meals.js
+++ backend/api/meals.js
@@ -1,10 +1,25 @@
-// backend/api/meals.js
 import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
-    const data = await fs.readFile("./data/available-meals.json", "utf8");
-    res.status(200).json(JSON.parse(data));
+    try {
+      const filePath = path.join(
+        __dirname,
+        "..",
+        "data",
+        "available-meals.json"
+      );
+      const data = await fs.readFile(filePath, "utf8");
+      res.status(200).json(JSON.parse(data));
+    } catch (error) {
+      console.error("Error reading meals file:", error);
+      res.status(500).json({ message: "Server error" });
+    }
   } else {
     res.status(405).json({ message: "Method not allowed" });
   }
